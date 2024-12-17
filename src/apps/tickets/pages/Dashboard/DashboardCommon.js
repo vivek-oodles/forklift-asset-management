f0f0e056f7417b776c6cdcd472acafb5eec31af3
nileshoodles <nilesh.kumar@oodles.io>
2024-12-17 12:04:19 +0530

    Role code added to the dashboard common

--- src/apps/tickets/pages/Dashboard/DashboardCommon.js
+++ src/apps/tickets/pages/Dashboard/DashboardCommon.js
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import AdminDashboard from './AdminDashboard';
-import AssetManagerDashboard from './AssetManagerDashboard';
-import CustomerManagerDashboard from './CustomerManagerDashboard';
-import UserDashboard from './UserDashboard';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import AdminDashboard from "./AdminDashboard";
+import AssetManagerDashboard from "./AssetManagerDashboard";
+import CustomerManagerDashboard from "./CustomerManagerDashboard";
+import UserDashboard from "./UserDashboard";
+// import { useLocation } from "react-router-dom";
 
 export const DashboardCommon = () => {
-    const location = useLocation()
-    const role = location.state.role;
+  // const location = useLocation()
+  // const role = location.state.role;
+  const role = localStorage.getItem("userRole");
 
   return (
-    <div id='dashboard'>
-        {
-        role ==='admin'?
-               <AdminDashboard/>//admin component
-                :
-                role === 'asset_manager'?
-                      <AssetManagerDashboard/> //asset manager component
-                    :
-                    // role === 'new'? //comp:
-                role === 'customer_manager'?
-                      <CustomerManagerDashboard/>
-                    :
-                  <UserDashboard/> //user Component
-         
-        }
+    <div id="dashboard">
+      {
+        role === "admin" ? (
+          <AdminDashboard /> //admin component
+        ) : role === "asset_manager" ? (
+          <AssetManagerDashboard /> //asset manager component
+        ) : // role === 'new'? //comp:
+        role === "customer_manager" ? (
+          <CustomerManagerDashboard />
+        ) : (
+          <UserDashboard />
+        ) //user Component
+      }
     </div>
-  )
-}
+  );
+};
