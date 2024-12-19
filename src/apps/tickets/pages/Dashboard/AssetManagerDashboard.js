c2fb4764c45db30e948da50cd0c4c0777f81d135
nileshoodles <nilesh.kumar@oodles.io>
2024-12-19 17:10:20 +0530

    View Data saperate component created.

--- src/apps/tickets/pages/Dashboard/AssetManagerDashboard.js
+++ src/apps/tickets/pages/Dashboard/AssetManagerDashboard.js
@@ -1,96 +1,26 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import {
-  FaSearch,
-  FaEye,
-  FaCubes,
-  FaCalendarAlt,
-  FaExclamationTriangle,
-  FaShieldAlt,
-  FaTimes,
-  FaEdit,
-  FaTrash,
-  FaFile,
-  FaDownload,
-  FaPlus,
-} from "react-icons/fa";
+import { FaEye, FaEdit, FaTrash, FaPlus } from "react-icons/fa";
 import "./AssetManagerDashboard.css";
 import { API_END_POINTS } from "../../../../network/apiEndPoint";
 import Pagination from "../../../../SharedComponent/Pagination";
 import useDebounce from "../../../../hooks/useDebounce";
-import InputField from "../../../../SharedComponent/Fields/InputField";
-import TextareaField from "../../../../SharedComponent/Fields/TextareaField";
-import SelectField from "../../../../SharedComponent/Fields/SelectField";
-import Button from "../../../../SharedComponent/Button/Button";
 import StatCard from "../../../../SharedComponent/Dashboard/StatCard";
-import {
-  deleteProtected,
-  getProtected,
-  patchProtected,
-} from "../../../../network/ApiService";
+import { deleteProtected, getProtected } from "../../../../network/ApiService";
 import { objectToQueryParams } from "../../../../utils/commonHelper";
 import { toast } from "react-toastify";
 import CreateAssetModal from "../../../../components/common/CreateAssetModal";
-import EditAssetModal from "../../../../components/common/AssetUpdateModal";
 import AssetUpdateModal from "../../../../components/common/AssetUpdateModal";
 import style from "./AssetManagerDashboard.module.css";
-
-const initialAssests = {
-  description: "",
-  brand: "",
-  model: "",
-  machine_category: 0,
-  warehouse: 0,
-  year_of_manufacture: 0,
-  lift_height: 0,
-  capacity: 0,
-  mast: "Triple",
-  closed_height: 0,
-  purchase_date: "",
-  purchase_price: 0,
-  battery: "4000 mah",
-  battery_description: "0",
-  battery_charge_due_date: "",
-  locations: [1],
-  status: "",
-  condition: "New",
-  operating_hours: 0,
-  maintenance_schedule: "",
-  maintenance_costs: 0,
-  warranty: "1 year",
-  warranty_expiration_date: "",
-  operational_status: "",
-  fuel_type: 0,
-  fuel_consumption: 0,
-  uvv_due_date: "",
-  notes: "",
-  depreciation_method: 1,
-  useful_life: 0,
-  residual_value: 0,
-  lease_company: "",
-  annual_depreciation_cost: 0,
-  total_operating_costs: 0,
-  serial_number: "",
-};
+import ViewData from "../../../../components/common/ViewData";
 
 const limit = 5;
 
 const AssetManagerDashboard = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
   const [assetCount, setAssetCount] = useState(0);
-  const [overDueMaintenance, setOverDueMaintenance] = useState(0);
-  const [currMonthCount, setCurrMonthCount] = useState(0);
-  const [warranty, setWarranty] = useState(0);
   const [showEditModal, setShowEditModal] = useState(false);
   const [selectedAsset, setSelectedAsset] = useState({});
-  const [showViewModal, setShowViewModal] = useState(false);
   const [showLoader, setShowLoader] = useState(true);
-  const [isViewLoading, setIsViewLoading] = useState(false);
-  const [showCreateAssetModal, setShowCreateAssetModal] = useState(false);
   const [assets, setAssets] = useState([]);
-  const [showCreateAssetsModal, setShowCreateAssetsModal] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
   const [filters, setFilters] = useState({
@@ -99,27 +29,8 @@ const AssetManagerDashboard = () => {
     category: "",
   });
   const [CurrentPage, setCurrentPage] = useState(0);
-  const [newAsset, setNewAsset] = useState({ ...initialAssests });
   const [searchQuery, setSearchQuery] = useState("");
-  const [dashboardData, setDashboardData] = useState({
-    total_assets: 0,
-    assets_under_maintenance: 0,
-    used_status: 0,
-    miete: 0,
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setNewAsset((pre) => ({ ...pre, [name]: value }));
-  };
-
-  // Retrieve user role from local storage or context
-  const [userRole, setUserRole] = useState("");
-
-  useEffect(() => {
-    const role = localStorage.getItem("userRole"); // Assuming role is stored in local storage
-    setUserRole(role);
-  }, []);
+  const [viewAssetId, setViewAssetId] = useState(null);
 
   // Fetch assigned assets
   const fetchAssets = async () => {
@@ -169,35 +80,6 @@ const AssetManagerDashboard = () => {
     }
   }, [debouncedQuery]);
 
-  useEffect(() => {
-    getDashboard();
-  }, []);
-
-  const getDashboard = async () => {
-    try {
-      const url = API_END_POINTS.dashboard;
-      const response = await getProtected(url);
-      if (response) {
-        setDashboardData({
-          total_assets: response?.total_assets,
-          assets_under_maintenance: response?.assets_under_maintenance,
-          used_status:
-            response?.assets_by_status?.find((item) => item?.status === "Used")
-              ?.count || 0,
-          miete:
-            response?.assets_by_status?.find((item) => item?.status === "Miete")
-              ?.count || 0,
-        });
-      }
-    } catch (e) {
-      console.log(e);
-      const message = e?.response?.data?.messages;
-      if (message && message?.[0]?.message) {
-        toast.error(message[0].message);
-      }
-    }
-  };
-
   // Handle asset deletion
   const handleDeleteAsset = async (asset) => {
     if (!window.confirm("Are you sure you want to delete this asset?")) {
@@ -216,27 +98,6 @@ const AssetManagerDashboard = () => {
       if (message && message?.[0]?.message) {
         toast.error(message[0].message);
       }
-    }
-  };
-
-  //// view
-
-  const handleViewAsset = async (asset) => {
-    setShowViewModal(true);
-    setIsViewLoading(true);
-
-    const token = localStorage.getItem("access");
-    try {
-      const response = await axios.get(`${API_END_POINTS.assets}${asset}/`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      setSelectedAsset(response.data);
-      console.log("hello this is daa", response.data);
-    } catch (error) {
-      console.error("Error fetching asset details:", error);
-      alert("Failed to load asset details. Please try again.");
-    } finally {
-      setIsViewLoading(false);
     }
   };
 
@@ -322,13 +183,6 @@ const AssetManagerDashboard = () => {
                           : "new"
                       ]
                     }`}
-                    // style={{
-                    //   width: "max-content",
-                    //   padding: "2px 10px",
-                    //   borderRadius: "50px",
-                    //   backgroundColor: "rgb(191 223 251)",
-                    //   color: "#0052cc",
-                    // }}
                   >
                     {asset.status === "Miete"
                       ? "Short Term Hired"
@@ -344,7 +198,7 @@ const AssetManagerDashboard = () => {
                   <div className={style["action-buttons"]}>
                     <button
                       className={style["btn-view"]}
-                      onClick={() => handleViewAsset(asset.id)}
+                      onClick={() => setViewAssetId(asset.id)}
                     >
                       <FaEye />
                     </button>
@@ -383,71 +237,20 @@ const AssetManagerDashboard = () => {
             itemOffset={CurrentPage}
             limit={limit}
           />
-          {/* <button onClick={prev}>Previous</button>
-          <button onClick={next}>Next</button> */}
         </div>
       </div>
 
-      {/* Modal Components - Edit, View, Create */}
-       <div>
-      {/* Button to open the edit modal */}
-      {/* <button onClick={() => setShowEditModal(true)}>Edit Asset</button> */}
-
-      {/* Asset Update Modal */}
-      <AssetUpdateModal
-        selectedAsset={selectedAsset}
-        setSelectedAsset={setSelectedAsset}
-        setShowEditModal={setShowEditModal}
-        showEditModal={showEditModal}
-        setAssets={setAssets}
-      />
-    </div>
-
-      {showViewModal && (
-        <div className="modal-backdrop">
-          <div className="modal-content">
-            {isViewLoading ? (
-              <div className="modal-loader">
-                <div className="loader-spinner"></div>
-                <p>Loading asset details...</p>
-              </div>
-            ) : (
-              <div>
-                <div className="modal-header">
-                  <h2>Asset Details</h2>
-                  <button
-                    className="close-button"
-                    onClick={() => setShowViewModal(false)}
-                  >
-                    <FaTimes />
-                  </button>
-                </div>
-                <div className="modal-body">
-                  {selectedAsset ? (
-                    Object.entries(selectedAsset).map(([key, value]) => {
-                      if (
-                        key === "created_at" ||
-                        key === "updated_at" ||
-                        key === "id" ||
-                        key === "attachments"
-                      ) {
-                        return null;
-                      }
-                      return (
-                        <p key={key}>
-                          <strong>{formatKey(key)}:</strong> {value}
-                        </p>
-                      );
-                    })
-                  ) : (
-                    <p>No details available for this asset.</p>
-                  )}
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-      )}
+      <div>
+        {/* Asset Update Modal */}
+        <AssetUpdateModal
+          selectedAsset={selectedAsset}
+          setSelectedAsset={setSelectedAsset}
+          setShowEditModal={setShowEditModal}
+          showEditModal={showEditModal}
+          setAssets={setAssets}
+        />
+      </div>
+      <ViewData id={viewAssetId} />
       {/* Create Asset Modal */}
       <CreateAssetModal show={showModal} onClose={setShowModal} />
     </div>
@@ -455,9 +258,3 @@ const AssetManagerDashboard = () => {
 };
 
 export default AssetManagerDashboard;
-
-const formatKey = (key) => {
-  return key
-    .replace(/_/g, " ") // Replace underscores with spaces
-    .replace(/\b\w/g, (char) => char.toUpperCase()); // Capitalize first letter of each word
-};
---
+++ src/components/common/ViewData.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { FaTimes } from "react-icons/fa";
+import { getProtected } from "../../network/ApiService";
+import { API_END_POINTS } from "../../network/apiEndPoint";
+
+const ViewData = (props) => {
+  const { id } = props;
+  const [selectedAsset, setSelectedAsset] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [showModal, setShowModal] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      handleViewAsset(id);
+    }
+  }, [id]);
+
+  const handleViewAsset = async (id) => {
+    setShowModal(true);
+    setLoading(true);
+
+    try {
+      const response = await getProtected(`${API_END_POINTS.assets}${id}/`);
+      if (response) {
+        console.log("this is data",response);
+        setSelectedAsset(response);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error("Error fetching asset details:", error);
+      alert("Failed to load asset details. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return showModal ? (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        {loading ? (
+          <div className="modal-loader">
+            <div className="loader-spinner"></div>
+            <p>Loading asset details...</p>
+          </div>
+        ) : (
+          <div>
+            <div className="modal-header">
+              <h2>Asset Details</h2>
+              <button
+                className="close-button"
+                onClick={() => setShowModal(false)}
+              >
+                <FaTimes />
+              </button>
+            </div>
+            <div className="modal-body">
+              {/* Display asset details here */}
+              {selectedAsset ? (
+                Object.entries(selectedAsset).map(([key, value]) => {
+                  if (
+                    key === "created_at" ||
+                    key === "updated_at" ||
+                    key === "id" ||
+                    key === "attachments"
+                  ) {
+                    return null;
+                  }
+                  return (
+                    <p key={key}>
+                      <strong>{formatKey(key)}:</strong> {value}
+                    </p>
+                  );
+                })
+              ) : (
+                <p>No details available for this asset.</p>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  ) : null;
+};
+
+export default ViewData;
+
+const formatKey = (key) => {
+  return key
+    .replace(/_/g, " ") // Replace underscores with spaces
+    .replace(/\b\w/g, (char) => char.toUpperCase()); // Capitalize first letter of each word
+};
